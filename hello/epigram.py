a91838bfd6e80995364734e6e3b92c791a8d06d4
Fasuto1st <60424518+Fasuto1st@users.noreply.github.com>
2020-02-05 14:52:21 +0700

    Add phrase

--- hello/epigram.py
+++ hello/epigram.py
@@ -1,4 +1,6 @@
 msg = "Great men are not born great, they grow great."
 msg2 = "Mario Puzo, The Godfather"
+msg3 = "Do, or do not. There is no try."
+msg4 = "Yoda, Star Wars"
 print(msg)
 print(msg2)
